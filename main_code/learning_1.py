e4366af0d7f7262310381773e68ae1553770322a
DanielWard325 <daniel_ward325@hotmail.co.uk>
2014-09-27 13:14:37 +0100

    adding stuff

--- main_code/learning_1.py
+++ main_code/learning_1.py
@@ -2,3 +2,138 @@ __author__ = 'Daniel'
 
 
 print('hello kate')
+
+
+string = 'string' "string"
+
+numbers = 5, 4.6
+
+lists = ['bob','kate'], [2,3,4,5,6], ['bob', 1, [1,2,3,4,]]
+
+example = [1,2,3,4,[5,6,7,8]]
+
+print(example[0])
+print(example[4])
+print(example[4][2])
+
+dictionaries = {'apple': {'cals':50},
+                'bob': 'kate'}
+
+print(dictionaries['apple']['cals'])
+print(dictionaries.items())
+
+
+
+
+sets = {1,2,3,4,5,6}
+
+
+
+
+
+
+x = 10
+
+while x > 10:
+
+    print(x)
+    x += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+string = 'pie'
+
+number = 1,2,3,4
+
+floats = 3.7
+
+lists = ['apple', 'blueberry', 'rhubarb', ['custard', 'cream']]
+
+print(lists [1])
+print(lists[0])
+print(lists[3][1])
+
+dictionaries = {'apple': ['gala','braeburn', 'pink lady'],
+                'berries': ['raspberry', 'blueberry', 'blackberry']}
+
+print(dictionaries['apple'][2])
+
+
+variable_list = [string, number, lists, dictionaries]
+
+print(variable_list[2][2])
+
+tupples = ('fffff', 'nnnnn')
+
+
+monkey = [1,2,3,4]
+def kates_smelly_bum(listy, b):
+    listy.append(b)
+
+kates_smelly_bum(monkey, 'boots')
+kates_smelly_bum(monkey, 'sword')
+
+print(monkey)
+
+def complex_function(x, y):
+    z = x + y
+    kates_smelly_bum(monkey, 'gems')
+    return z
+
+def health_modifer(original, additional):
+    x = original + additional
+    return x
+
+health = health_modifer(60, 10)
+print(health)
+
+#function_example()
+#function_example()
+varfunc = complex_function(5, 7)
+print(monkey)
+print(varfunc)
+
+bobby = function_example()
+
+print(bobby)
